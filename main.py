a4f02537aff4e7e2af1a36fd7b937199e93225ce
Vadim Salomatin <vadim.d.salomatin@gmail.com>
2023-02-26 19:27:42 +0300

    Сделана задача 'Капучино'

--- main.py
+++ main.py
@@ -2,8 +2,72 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class LoadWidget(QDialog):
+    def __init__(self, parent, con, id, name, stepen, molotiy, vkys, cost, volume, description):
+        super().__init__(parent)
+        self.con = con
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.lineEdit.setText(str(id))
+        self.lineEdit_2.setText(str(name))
+        self.lineEdit_3.setText(str(stepen))
+        self.lineEdit_4.setText(str(molotiy))
+        self.lineEdit_5.setText(str(vkys))
+        self.lineEdit_6.setText(str(cost))
+        self.lineEdit_7.setText(str(volume))
+        self.lineEdit_8.setText(str(description))
+
+        self.saveButton.clicked.connect(self.save)
+
+    def save(self):
+        try:
+            if len(self.lineEdit.text()):
+                id = self.lineEdit.text()
+            else:
+                id = None
+            name = self.lineEdit_2.text()
+            stepen = self.lineEdit_3.text()
+            molotiy = self.lineEdit_4.text()
+            vkys = self.lineEdit_5.text()
+            cost = self.lineEdit_6.text()
+            volume = self.lineEdit_7.text()
+            description = self.lineEdit_8.text()
+            cur = self.con.cursor()
+            cur.execute(f"""
+                    INSERT INTO coffee 
+                    (
+                       id,
+                       name,
+                       stepen,
+                       molotiy,
+                       vkys,
+                       cost,
+                       volume,
+                       description
+                   )
+                   VALUES (?,?,?,?,?,?,?,?)
+                    ON CONFLICT(id) DO
+                    UPDATE SET name = ?,
+                       stepen = ?,
+                       molotiy = ?,
+                       vkys = ?,
+                       cost = ?,
+                       volume = ?,
+                       description = ?
+                    """,
+                        (
+                            id, name, stepen, molotiy, vkys, cost, volume, description, name, stepen,
+                            molotiy,
+                            vkys, cost, volume, description)
+                        )
+            self.con.commit()
+            self.accept()
+        except Exception as e:
+            print(e)
+            self.error_message.setText('Неверно заполнена форма')
 
 
 class MyWidget(QMainWindow):
@@ -11,19 +75,36 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.con = sqlite3.connect("coffee.sqlite")
-        self.con.set_trace_callback(print)
-        self.cur = self.con.cursor()
+        self.tableWidget.clicked.connect(self.select_row)
+        self.pushButton.clicked.connect(self.add_row)
+        self.load_table()
+
+    def select_row(self, item):
+        values = []
+        for i in range(self.tableWidget.columnCount()):
+            values.append(self.tableWidget.item(item.row(), i).text())
+        dialog = LoadWidget(self, self.con, *values)
+        if dialog.exec_() == QDialog.Accepted:
+            self.load_table()
+
+    def add_row(self):
+        values = [""] * 8
+        dialog = LoadWidget(self, self.con, *values)
+        if dialog.exec_() == QDialog.Accepted:
+            pass
         self.load_table()
 
     def get_data(self):
         q = """SELECT * FROM coffee"""
-        return self.cur.execute(q).fetchall()
+        return self.con.cursor().execute(q).fetchall()
 
     def load_table(self):
         try:
+            self.tableWidget.setRowCount(0);
             result = self.get_data()
-            self.tableWidget.setColumnCount(7)
-            self.tableWidget.setHorizontalHeaderLabels(['id', 'name', 'stepen', 'molotiy', 'vkys', 'cost', 'volume'])
+            self.tableWidget.setColumnCount(8)
+            self.tableWidget.setHorizontalHeaderLabels(
+                ['id', 'name', 'stepen', 'molotiy', 'vkys', 'cost', 'volume', 'description'])
             self.tableWidget.setRowCount(len(result))
             for i, row in enumerate(result):
                 for j, elem in enumerate(row):
